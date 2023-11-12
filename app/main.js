67c35cc1543eee8e3669f4a36ee6ef62c3ff0292
Igor Radovanov <igor@radovanovigor.com>
2023-11-12 20:04:50 +0100

    Implement multi-requests

--- app/main.js
+++ app/main.js
@@ -1,8 +1,5 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 class httpParser {
 
     constructor(dataBuffer) {
